9a621def21a6c82a582347a3c1ef80b13dd3af00
Hilary505 <omondihilary19@gmail.com>
2026-03-25 15:32:03 +0300

    modify : empoyeecontroller.java file

--- src/main/java/com/company/controller/EmployeeController.java
+++ src/main/java/com/company/controller/EmployeeController.java
@@ -10,6 +10,7 @@ import org.springframework.web.bind.annotation.*;
 import java.math.BigDecimal;
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/api/employees")
 public class EmployeeController {
